83d5fc5f9d52b77b175cbd24608927149b6383c7
Gummibeer <dev.gummibeer@gmail.com>
2019-08-04 20:48:15 +0200

    add validation messages

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'enum' => 'The :attribute field is not a valid :enum.',
+    'enum_index' => 'The :attribute field is not a valid index of :enum.',
+    'enum_name' => 'The :attribute field is not a valid name of :enum.',
+    'enum_value' => 'The :attribute field is not a valid value of :enum.',
+];
