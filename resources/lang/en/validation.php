fd86a05d14166b2e67670f678c6a3a5837ae67e9
Gummibeer <dev.gummibeer@gmail.com>
2019-08-05 21:09:50 +0200

    add example enums value translations

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -5,4 +5,11 @@ return [
     'enum_index' => 'The :attribute field is not a valid index of :enum.',
     'enum_name' => 'The :attribute field is not a valid name of :enum.',
     'enum_value' => 'The :attribute field is not a valid value of :enum.',
+
+    'enums' => [
+        \Spatie\Enum\Enumerable::class => [
+            'slugged_name' => 'translated value',
+            'slugged_other_name' => 'translated other value',
+        ],
+    ],
 ];
