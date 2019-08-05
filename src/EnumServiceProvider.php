42c7c413b01efc40bdc54c0a9f27520f85f6d807
Gummibeer <dev.gummibeer@gmail.com>
2019-08-05 20:35:02 +0200

    install translations

--- src/EnumServiceProvider.php
+++ src/EnumServiceProvider.php
@@ -7,6 +7,15 @@ use Spatie\Enum\Laravel\Commands\MakeEnum;
 
 class EnumServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/enum'),
+        ]);
+
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang/', 'enum');
+    }
+
     public function register()
     {
         $this->app->bind('command.make:enum', MakeEnum::class);
