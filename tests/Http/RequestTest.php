19f6ddbe0e7fe7f9b9972f962e01085e071a5540
Gummibeer <dev.gummibeer@gmail.com>
2019-08-06 15:01:43 +0200

    add request macro test

---
+++ tests/Http/RequestTest.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Spatie\Enum\Laravel\Tests\Http;
+
+use Illuminate\Http\Request;
+use Spatie\Enum\Laravel\Tests\Extra\LocaleEnum;
+use Spatie\Enum\Laravel\Tests\Extra\StatusEnum;
+use Spatie\Enum\Laravel\Tests\TestCase;
+
+final class RequestTest extends TestCase
+{
+    /** @test */
+    public function it_has_transform_enums_macro()
+    {
+        $this->assertTrue(Request::hasMacro('transformEnums'));
+    }
+
+    /** @test */
+    public function it_can_transform_a_request_get_parameter_to_enum_by_name()
+    {
+        $request = $this->createRequest(
+            [
+                'status' => 'draft',
+            ]
+        );
+
+        $request->transformEnums([
+            'query' => [
+                'status' => StatusEnum::class,
+            ],
+        ]);
+
+        $this->assertInstanceOf(StatusEnum::class, $request->query->get('status'));
+        $this->assertEquals('DRAFT', $request->query->get('status')->getName());
+    }
+
+    /** @test */
+    public function it_can_transform_a_request_get_parameter_to_enum_by_index()
+    {
+        $request = $this->createRequest(
+            [
+                'status' => 1,
+            ]
+        );
+
+        $request->transformEnums([
+            'query' => [
+                'status' => StatusEnum::class,
+            ],
+        ]);
+
+        $this->assertInstanceOf(StatusEnum::class, $request->query->get('status'));
+        $this->assertEquals('PUBLISHED', $request->query->get('status')->getName());
+    }
+
+    /** @test */
+    public function it_can_transform_a_request_get_parameter_to_enum_by_value()
+    {
+        $request = $this->createRequest(
+            [
+                'status' => 'stored archive',
+            ]
+        );
+
+        $request->transformEnums([
+            'query' => [
+                'status' => StatusEnum::class,
+            ],
+        ]);
+
+        $this->assertInstanceOf(StatusEnum::class, $request->query->get('status'));
+        $this->assertEquals('ARCHIVED', $request->query->get('status')->getName());
+    }
+
+    /** @test */
+    public function it_does_not_transform_a_request_get_parameter_to_enum_if_no_query_rule_present()
+    {
+        $request = $this->createRequest(
+            [
+                'status' => 'draft',
+            ]
+        );
+
+        $request->transformEnums([
+            'request' => [
+                'status' => StatusEnum::class,
+            ],
+        ]);
+
+        $this->assertIsString($request->query->get('status'));
+        $this->assertEquals('draft', $request->query->get('status'));
+    }
+
+    /** @test */
+    public function it_can_transform_a_request_post_parameter_to_enum_by_name()
+    {
+        $request = $this->createRequest(
+            [],
+            Request::METHOD_POST,
+            [
+                'status' => 'draft',
+            ]
+        );
+
+        $request->transformEnums([
+            'request' => [
+                'status' => StatusEnum::class,
+            ],
+        ]);
+
+        $this->assertInstanceOf(StatusEnum::class, $request->request->get('status'));
+        $this->assertEquals('DRAFT', $request->request->get('status')->getName());
+    }
+
+    /** @test */
+    public function it_can_transform_a_request_post_parameter_to_enum_by_index()
+    {
+        $request = $this->createRequest(
+            [],
+            Request::METHOD_POST,
+            [
+                'status' => 1,
+            ]
+        );
+
+        $request->transformEnums([
+            'request' => [
+                'status' => StatusEnum::class,
+            ],
+        ]);
+
+        $this->assertInstanceOf(StatusEnum::class, $request->request->get('status'));
+        $this->assertEquals('PUBLISHED', $request->request->get('status')->getName());
+    }
+
+    /** @test */
+    public function it_can_transform_a_request_post_parameter_to_enum_by_value()
+    {
+        $request = $this->createRequest(
+            [],
+            Request::METHOD_POST,
+            [
+                'status' => 'stored archive',
+            ]
+        );
+
+        $request->transformEnums([
+            'request' => [
+                'status' => StatusEnum::class,
+            ],
+        ]);
+
+        $this->assertInstanceOf(StatusEnum::class, $request->request->get('status'));
+        $this->assertEquals('ARCHIVED', $request->request->get('status')->getName());
+    }
+
+    /** @test */
+    public function it_can_transform_a_request_route_parameter_to_enum()
+    {
+        $request = $this->createRequest();
+
+        $request->transformEnums([
+            'route' => [
+                'locale' => LocaleEnum::class,
+            ],
+        ]);
+
+        $this->assertInstanceOf(LocaleEnum::class, $request->route('locale'));
+        $this->assertEquals('EN', $request->route('locale')->getName());
+        $this->assertEquals('en', $request->route('locale')->getValue());
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace Spatie\Enum\Laravel\Tests;
 
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
 use Spatie\Enum\Laravel\Tests\Extra\Post;
 use Spatie\Enum\Laravel\EnumServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
@@ -41,5 +43,37 @@ abstract class TestCase extends OrchestraTestCase
     public function getStub(string $file): string
     {
         return file_get_contents(__DIR__.'/stubs/'.$file);
+    }
+
+    protected function createRequest(
+        array $query = [],
+        string $method = Request::METHOD_GET,
+        array $content = []
+    ): Request {
+        $uri = 'http://localhost/en/test';
+        $server = [
+            'REQUEST_URI' => $uri,
+            'CONTENT_TYPE' => 'application/json',
+        ];
+
+        $request = new Request(
+            $query,
+            [],
+            [],
+            [],
+            [],
+            $server,
+            json_encode($content)
+        );
+        $request->setMethod($method);
+
+        $route = (new Route($method, '{locale}/test', [
+            'uses' => 'Controller@index',
+        ]))->bind($request);
+        $request->setRouteResolver(function () use ($route) {
+            return $route;
+        });
+
+        return $request;
     }
 }
