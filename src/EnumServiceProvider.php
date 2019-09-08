24664f0dda8e278f6032a84d35814bc83b1fb0b0
Gummibeer <dev.gummibeer@gmail.com>
2019-09-08 19:47:20 +0200

    register validation rule string alias

--- src/EnumServiceProvider.php
+++ src/EnumServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace Spatie\Enum\Laravel;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Enum\Laravel\Commands\MakeEnum;
+use Spatie\Enum\Laravel\Rules\Enum;
+use Spatie\Enum\Laravel\Rules\EnumIndex;
+use Spatie\Enum\Laravel\Rules\EnumName;
+use Spatie\Enum\Laravel\Rules\EnumValue;
+use Illuminate\Contracts\Validation\Validator as ValidatorContract;
 
 class EnumServiceProvider extends ServiceProvider
 {
@@ -14,6 +20,8 @@ class EnumServiceProvider extends ServiceProvider
         ]);
 
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang/', 'enum');
+
+        $this->bootValidationRules();
     }
 
     public function register()
@@ -23,5 +31,32 @@ class EnumServiceProvider extends ServiceProvider
         $this->commands([
             'command.make:enum',
         ]);
+    }
+
+    public function bootValidationRules(): void
+    {
+        Validator::extend('enum', function (string $attribute, $value, array $parameters, ValidatorContract $validator): bool {
+            $enum = $parameters[0] ?? null;
+
+            return (new Enum($enum))->passes($attribute, $value);
+        });
+
+        Validator::extend('enum_index', function (string $attribute, $value, array $parameters, ValidatorContract $validator): bool {
+            $enum = $parameters[0] ?? null;
+
+            return (new EnumIndex($enum))->passes($attribute, $value);
+        });
+
+        Validator::extend('enum_name', function (string $attribute, $value, array $parameters, ValidatorContract $validator): bool {
+            $enum = $parameters[0] ?? null;
+
+            return (new EnumName($enum))->passes($attribute, $value);
+        });
+
+        Validator::extend('enum_value', function (string $attribute, $value, array $parameters, ValidatorContract $validator): bool {
+            $enum = $parameters[0] ?? null;
+
+            return (new EnumValue($enum))->passes($attribute, $value);
+        });
     }
 }
