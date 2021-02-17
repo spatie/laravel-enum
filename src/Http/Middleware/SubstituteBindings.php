c8827aea5f37118fd7b9d852aeb6aec15a58d563
Tom Witkowski <dev.gummibeer@gmail.com>
2021-02-17 20:44:00 +0100

    move ImplicitRouteBinding logic into middleware instead of dedicated class

--- src/Http/Middleware/SubstituteBindings.php
+++ src/Http/Middleware/SubstituteBindings.php
@@ -2,24 +2,15 @@
 
 namespace Spatie\Enum\Laravel\Http\Middleware;
 
+use BadMethodCallException;
 use Closure;
-use Illuminate\Container\Container;
-use Spatie\Enum\Laravel\Routing\ImplicitRouteBinding;
+use Illuminate\Support\Reflector;
+use Illuminate\Support\Str;
+use Spatie\Enum\Enum;
+use Spatie\Enum\Laravel\Exceptions\InvalidEnumValueException;
 
 class SubstituteBindings
 {
-    /**
-     * The container instance.
-     *
-     * @var \Illuminate\Container\Container
-     */
-    private $container;
-
-    public function __construct(Container $container)
-    {
-        $this->container = $container;
-    }
-
     /**
      * @param \Illuminate\Http\Request $request
      * @param \Closure $next
@@ -27,8 +18,48 @@ class SubstituteBindings
      */
     public function handle($request, Closure $next)
     {
-        ImplicitRouteBinding::resolveForRoute($this->container, $request->route());
+        $route = $request->route();
+
+        $parameters = $route->parameters();
+
+        foreach ($route->signatureParameters(Enum::class) as $parameter) {
+            $parameterName = static::getParameterName($parameter->getName(), $parameters);
+            if ($parameterName === null) {
+                continue;
+            }
+
+            $parameterValue = $parameters[$parameterName];
+            if ($parameterValue instanceof Enum) {
+                continue;
+            }
+
+            try {
+                $instance = forward_static_call(
+                    /** @see \Spatie\Enum\Laravel\Enum::make() */
+                    [Reflector::getParameterClassName($parameter), 'make'],
+                    $parameterValue
+                );
+            } catch (BadMethodCallException $e) {
+                throw new InvalidEnumValueException($e->getMessage(), $e);
+            }
+
+            $route->setParameter($parameterName, $instance);
+        }
 
         return $next($request);
+    }
+
+    protected function getParameterName(string $name, array $parameters): ?string
+    {
+        if (array_key_exists($name, $parameters)) {
+            return $name;
+        }
+
+        $snakedName = Str::snake($name);
+        if (array_key_exists($snakedName, $parameters)) {
+            return $snakedName;
+        }
+
+        return null;
     }
 }
