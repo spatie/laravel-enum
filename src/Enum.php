a1d1ca31b09ffd7101cdb46883176ecd4413fee9
thecaliskan <13554944+thecaliskan@users.noreply.github.com>
2021-04-19 17:11:32 +0300

    Added Macroable

--- src/Enum.php
+++ src/Enum.php
@@ -5,12 +5,15 @@ namespace Spatie\Enum\Laravel;
 use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Support\Traits\Macroable;
 use Spatie\Enum\Enum as BaseEnum;
 use Spatie\Enum\Laravel\Casts\EnumCast;
 use Spatie\Enum\Laravel\Casts\EnumCollectionCast;
 
 abstract class Enum extends BaseEnum implements Jsonable, Castable
 {
+    use Macroable;
+
     public static function castUsing(array $arguments): CastsAttributes
     {
         if (in_array('collection', $arguments)) {
