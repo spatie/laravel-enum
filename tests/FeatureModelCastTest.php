7fea1b355e5348d443190cef684921a370ae3b40
danielpetrovaliev <daniel.petrovaliev@edentechlabs.io>
2021-05-30 12:07:16 +0300

    remove redundant use namespace

--- tests/FeatureModelCastTest.php
+++ tests/FeatureModelCastTest.php
@@ -2,7 +2,6 @@
 
 namespace Spatie\Enum\Laravel\Tests;
 
-use Spatie\Enum\Laravel\Exceptions\NotNullableEnumField;
 use Spatie\Enum\Laravel\Tests\Extra\Post;
 use Spatie\Enum\Laravel\Tests\Extra\StatusEnum;
 use stdClass;
